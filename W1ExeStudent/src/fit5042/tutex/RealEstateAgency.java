cad71daf88515e83cd31a0922e3dd471d27c45db
zdh <dzha0033@student.monash.edu>
2020-08-06 23:07:54 +1000

    3

--- W1ExeStudent/src/fit5042/tutex/RealEstateAgency.java
+++ W1ExeStudent/src/fit5042/tutex/RealEstateAgency.java
@@ -1,5 +1,64 @@
 package fit5042.tutex;
 
-public class RealEstateAgency {
+import fit5042.tutex.repository.PropertyRepository;
+import fit5042.tutex.repository.PropertyRepositoryFactory;
+import fit5042.tutex.repository.entities.Property;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ *
+ * TODO Exercise 1.3 Step 3 Complete this class. Please refer to tutorial instructions.
+ * This is the main driver class. This class contains the main method for Exercise 1A
+ * 
+ * This program can run without the completion of Exercise 1B.
+ * 
+ * @author Junyang
+ */
+public class RealEstateAgency {
+    private String name;
+    private final PropertyRepository propertyRepository;
+
+    public RealEstateAgency(String name) throws Exception {
+        this.name = name;
+        this.propertyRepository = PropertyRepositoryFactory.getInstance();
+    }
+    
+    // this method is for initializing the property objects
+    // complete this method
+    public void createProperty() {
+        
+    }
+    
+    // this method is for displaying all the properties
+    // complete this method
+    public void displayProperties() {
+        
+    }
+    
+    // this method is for searching the property by ID
+    // complete this method
+    public void searchPropertyById() {
+        
+    }
+    
+    public void run() {
+        createProperty();
+        System.out.println("********************************************************************************");
+        displayProperties();
+        System.out.println("********************************************************************************");
+        searchPropertyById();
+    }
+    
+    public static void main(String[] args) {
+        try {
+            new RealEstateAgency("FIT5042 Real Estate Agency").run();
+        } catch (Exception ex) {
+            System.out.println("Application fail to run: " + ex.getMessage());
+        }
+    }
 }
